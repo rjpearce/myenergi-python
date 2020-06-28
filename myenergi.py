538f1eab5550310b23bcd98e429382011c5b8d19
Richard Pearce <richard.pearce@automationlogic.com>
2020-06-28 13:06:42 +0100

    Split out zappi class, split web request and device creation

--- myenergi.py
+++ myenergi.py
@@ -7,15 +7,29 @@ from requests.auth import HTTPDigestAuth
 import yaml
 import json
 
+class zappi:
+  name = ''
+  attributes = {}
+
+  def __init__(self, attributes):
+    self.attributes = attributes
+    self.name = f'zappi_{attributes["sno"]}'
+
+  def __repr__(self):
+    return self.name
+
 class myenergi:
   """ Implements the myenergi API """
   config = {}
+  devices = []
   connect_timeout = 10
   read_timeout = 30
+  use_cache = True
 
   def __init__(self, config_path=f'{os.getcwd()}/config.yaml'):
     self.load_config(config_path)
     self.validate_config()
+    self.devices = self.populate_devices()
   
   def load_config(self, config_path):
     """ Loads the configuration file """
@@ -29,23 +43,31 @@ class myenergi:
     if 'hub_password' not in self.config:
       print('Hub password not found')
       raise Exception('hub_password missing from config')
+  
+    
 
-  def get_zappi_status(self):
-    """ Returns the current API status """
-
+  def request_status(self, endpoint='*'):
     hub_serial = self.config['hub_serial']
     hub_password = self.config['hub_password']
 
     base_url = f'https://s{hub_serial[-1]}.myenergi.net'
-    status_url = f'{base_url}/cgi-jstatus-*'
+    status_url = f'{base_url}/cgi-jstatus-{endpoint}'
 
     headers = {'User-Agent': 'Wget/1.14 (linux-gnu)'}
     auth = HTTPDigestAuth(hub_serial, hub_password)
     response = requests.get(status_url, headers=headers, auth=auth, timeout=(self.connect_timeout, self.read_timeout))
-    print(response.status_code)
-    status_file = open("status.json", "w")
-    status_file.write(json.dumps(response.json()))
-    status_file.close()
+    if response.status_code == 200:
+      status_file = open("status.json", "w")
+      status_file.write(json.dumps(response.json()))
+      status_file.close()
+      return response.json()
+
+  def get_all_devices(self):
+    """ Returns the current API status """
+    if self.use_cache:
+      with open('tests/fixtures/status.json') as data_file:    
+        return json.load(data_file)
+    return self.request_status('*')
 
   def translate_value(self, attribute, value, device):
     if device in ('zappi', 'eddi'):
@@ -62,16 +84,17 @@ class myenergi:
         return translations['attributes'].get(attribute, attribute)
     return attribute
 
-  def list_devices(self, response_json):
+  def populate_devices(self):
+    data = self.get_all_devices()
     devices = []
-    for device_list in response_json:
+    for device_list in data:
       for device, attributes_list in device_list.items():
         # Ignore asn
         if isinstance(attributes_list, (list)):
           # Ignore devices without any attributes
           if len(attributes_list) > 0:
-            if 'sno' in attributes_list[0]:
-              devices.append(f'{device}_{attributes_list[0]["sno"]}')
+            if device in ('zappi'):
+              devices.append(zappi(attributes_list[0]))
     return devices
 
   def translate_response(self, response_json):
@@ -93,11 +116,7 @@ class myenergi:
 
 def main(): 
   mye = myenergi()
-  #mye.get_zappi_status()
+  pprint(mye.devices)
 
-  with open('tests/fixtures/status.json') as data_file:    
-    data = json.load(data_file)
-    pprint(mye.list_devices(data))
-    pprint(mye.translate_response(data))
 if __name__ == "__main__":
   main()
