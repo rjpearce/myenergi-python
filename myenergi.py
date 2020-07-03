50229d44a9589e8018db57f7a3352a9e38dbb6d6
Richard Pearce <richard.pearce@automationlogic.com>
2020-07-03 23:32:33 +0100

    Initial implementation of get_boost_times

--- myenergi.py
+++ myenergi.py
@@ -22,14 +22,26 @@ class MyEnergiDevice:
     self.attributes = attributes
     self.name = f'{self.device}[{attributes["sno"]}]'
     self.id = f'{self.device[:1]}{attributes["sno"]}'
-    self.translated_attributes = self.translate_attributes()
+    self.translated_attributes = self.translate(self.attributes)
 
   def __repr__(self):
     return self.name
 
+  def translate_days_value(self, days):
+    weekdays = ['?', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    translated_days = []
+    for day in range(len(days)):
+      if days[day] == '1': 
+        translated_days.append(weekdays[day])
+    if (len(translated_days) == 0):
+      return 'None'
+    return ','.join(translated_days)
+
   def translate_value(self, attribute, value):
     with open(f'translations/{self.device}.yaml', 'r') as translation_file:
       translations = yaml.safe_load(translation_file.read())
+      if attribute == 'bdd':
+        return self.translate_days_value(value)
       if 'values' in translations:
         if attribute in translations['values']:
           return translations['values'][attribute].get(value, value)
@@ -41,10 +53,10 @@ class MyEnergiDevice:
       if 'attributes' in translations:
         return translations['attributes'].get(attribute, attribute)
     return attribute
-
-  def translate_attributes(self):
+ 
+  def translate(self, attributes):
     translated = {}
-    for attr, value in self.attributes.items():
+    for attr, value in attributes.items():
       tattr = self.translate_attribute(attr)
       tvalue = self.translate_value(attr, value)
       translated[tattr] = tvalue
@@ -57,10 +69,18 @@ class Eddi(MyEnergiDevice):
   pass
 
 class Zappi(MyEnergiDevice):
+  boost_times = []
+  translated_boost_times = []
 
-  def get_boost_time(self):
-   pass
-   #/cgi-boost-time-Z10077777
+  def get_boost_times(self, translate=True):
+    self.translated_boost_times = []
+    response_json = self.myenergi.api_request(f'{self.myenergi.base_url}/cgi-boost-time-{self.id}')
+    self.boost_times = response_json['boost_times']
+    for boost_time in response_json['boost_times']:
+      self.translated_boost_times.append(self.translate(boost_time))
+    if translate: 
+      return self.translated_boost_times
+    return self.boost_times
 
 class MyEnergi:
   """ Implements the myenergi API """
@@ -94,7 +114,6 @@ class MyEnergi:
     if 'hub_password' not in self.config:
       raise Exception('hub_password missing from config')
     if 'use_cache' not in self.config:
-      print('here')
       self.config['use_cache'] = False
   
   def api_request(self, url):
@@ -143,8 +162,10 @@ def main():
   mye = MyEnergi()
   mye.populate_devices()
   for device in mye.devices:
-    pprint(device.name)
-    pprint(device.translated_attributes)
+    if isinstance(device, (Zappi)):
+      pprint(device.get_boost_times())
+      #pprint(device.name)
+      #pprint(device.translated_attributes)
 
 if __name__ == "__main__":
   main()
